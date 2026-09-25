c75e545ca5fb60399dee63bafa57947961161fb7
GamePTBR <leofreitas.14@live.com>
2026-09-26 00:15:29 +0100

    Add translations for Risen 2 and Tower of Time

---
+++ traducoes.js
@@ -0,0 +1,29 @@
+const traducoes = [
+
+    {
+        nome: "Risen 2: Dark Waters",
+        pagina: "risen-2.html",
+        imagem: "Risen2.jpg",
+        plataforma: "PC",
+        idioma: "PT-BR",
+        versao: "v1.0",
+        status: "Concluída",
+        tamanho: "8,1 MB",
+        atualizado: "24/09/2026",
+        dataISO: "2026-09-24"
+    },
+
+    {
+        nome: "Tower of Time",
+        pagina: "tower-of-time.html",
+        imagem: "Tower.webp",
+        plataforma: "PC",
+        idioma: "PT-BR",
+        versao: "v1.1",
+        status: "Concluída",
+        tamanho: "254 MB",
+        atualizado: "24/09/2026",
+        dataISO: "2026-09-24"
+    }
+
+];
